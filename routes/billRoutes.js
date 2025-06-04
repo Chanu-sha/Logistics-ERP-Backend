dcae4d1edf4d4d9ce3d9bd555dbfc138883b0669
Chanu-sha <chanu2336@gmail.com>
2025-06-04 17:54:16 +0530

    Initial commit

---
+++ routes/billRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  createBill,
+  deleteBill,
+  getAllBills,
+  getBillById,
+  updateBill,
+} from "../controllers/billController.js";
+
+const router = express.Router();
+
+router.post("/", createBill);
+router.get("/", getAllBills);
+router.get("/:id", getBillById);
+router.put("/:id", updateBill);
+router.delete("/:id", deleteBill);
+
+export default router;
---
+++ server.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import billRoutes from "./routes/billRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));app.use(express.json());
+
+app.use("/api/bills", billRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Billing Backend is up and running.");
+});
+
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("✅ Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`🚀 Server listening on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection error:", err);
+    process.exit(1);
+  });
